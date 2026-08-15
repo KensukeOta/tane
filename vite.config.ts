d6ce1a3d7978cc012a24fa83bd68adcbbbbb7d8a
KensukeOta <bump_kensuke_miwa@yahoo.ne.jp>
2026-08-15 20:48:24 +0900

    chore: 開発・デプロイ環境ごとにViteのbase設定を切り替え

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/tane/",
+export default defineConfig(({ command, mode }) => {
+  if (command === "serve") {
+    return {
+      base: "/",
+    };
+  }
+
+  return {
+    base: mode === "holyland" ? "./" : "/tane/",
+  };
 });
